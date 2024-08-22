ed1df7bbbe152dd28da2a70664c6a3104a44e2f9
Eason Sy <112996391+SyKaHo@users.noreply.github.com>
2024-08-23 03:11:18 +0800

    new

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
-function openIG() {
-  window.open("https://www.instagram.com/easonsykaho/" , '_blank');
+function openGithub() {
+  window.open("https://github.com/easonsy0725", "_blank");
 }
 
-function openGithub() {
-  window.open("https:/github.com/easonsy0725" , '_blank');
+function openIG() {
+  window.open("https://www.instagram.com/easonsykaho/" , '_blank');
 }
